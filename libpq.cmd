c4eed58a23c945c6b1f74e8bb89c85063d92cfe6
Andrey Volk <andywolk@gmail.com>
2018-04-05 16:08:38 +0300

    Use shared zlib library instead of static.

--- libpq.cmd
+++ libpq.cmd
@@ -28,11 +28,21 @@ mkdir zlib\lib
 xcopy /s %~4\include zlib\include
 
 if "%~1" == "amd64" (
-copy %~4\binaries\x64\%~2\zlib.lib zlib\lib\zdll.lib
+  if "%~2" == "Debug" (
+      copy %~4\binaries\x64\%~2\lib\zlibd.lib zlib\lib\zdll.lib
+  )
+  if "%~2" == "Release" (
+      copy %~4\binaries\x64\%~2\lib\zlib.lib zlib\lib\zdll.lib
+  )
 )
 
 if "%~1" == "x86" ( 
-copy %~4\binaries\Win32\%~2\zlib.lib zlib\lib\zdll.lib
+  if "%~2" == "Debug" (
+      copy %~4\binaries\Win32\%~2\lib\zlibd.lib zlib\lib\zdll.lib
+  )
+  if "%~2" == "Release" (
+      copy %~4\binaries\Win32\%~2\lib\zlib.lib zlib\lib\zdll.lib
+  )
 )                                                         
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %~1
