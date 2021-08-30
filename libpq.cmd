b24c714e063e0f02b6765c1c350759d893d62565
Andrey Volk <andywolk@gmail.com>
2021-08-30 18:29:42 +0300

    Update libpq to 10.18 and use openssl 1.1.x

--- libpq.cmd
+++ libpq.cmd
@@ -1,22 +1,26 @@
 echo Building Platfrom = %~1 Configuration=%~2
-copy config.pl postgresql-10.3\src\tools\msvc\config.pl
+copy config.pl postgresql-10.18\src\tools\msvc\config.pl
+copy Solution.pm postgresql-10.18\src\tools\msvc\Solution.pm
 
 REM Copy openssl binaries
 RMDIR /S /Q openssl 2>nul
 mkdir openssl
 mkdir openssl\include
 mkdir openssl\lib
+mkdir openssl\bin
 echo %~3
 xcopy /s %~3\include openssl\include
 
 if "%~1" == "amd64" (
 xcopy /s %~3\include_x64 openssl\include
 xcopy /s %~3\binaries\x64\%~2 openssl\lib
+xcopy /s %~3\binaries\x64\%~2 openssl\bin
 )
 
 if "%~1" == "x86" ( 
 xcopy /s %~3\include_x86 openssl\include
 xcopy /s %~3\binaries\Win32\%~2 openssl\lib
+xcopy /s %~3\binaries\Win32\%~2 openssl\bin
 )
 
 REM Copy zlib binaries
@@ -45,6 +49,8 @@ if "%~1" == "x86" (
   )
 )                                                         
 
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %~1
-cd postgresql-10.3/src/tools/msvc/
+REM call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %~1
+
+call vc_env.cmd %~5 %~6
+cd postgresql-10.18/src/tools/msvc/
 build.bat %~2 libpq
